202991f7712646b1f3be5d0db4c5c9b6e549435c
Karim Mourra <karim@jwplayer.com>
2022-05-27 16:09:49 -0400

    setup:

--- exchange/adapter_builders.go
+++ exchange/adapter_builders.go
@@ -68,6 +68,7 @@ import (
 	"github.com/prebid/prebid-server/adapters/iqzone"
 	"github.com/prebid/prebid-server/adapters/ix"
 	"github.com/prebid/prebid-server/adapters/jixie"
+	"github.com/prebid/prebid-server/adapters/jwplayer"
 	"github.com/prebid/prebid-server/adapters/kayzen"
 	"github.com/prebid/prebid-server/adapters/kidoz"
 	"github.com/prebid/prebid-server/adapters/krushmedia"
@@ -201,6 +202,7 @@ func newAdapterBuilders() map[openrtb_ext.BidderName]adapters.Builder {
 		openrtb_ext.BidderIQZone:            iqzone.Builder,
 		openrtb_ext.BidderIx:                ix.Builder,
 		openrtb_ext.BidderJixie:             jixie.Builder,
+		openrtb_ext.BidderJWPlayer:          jwplayer.Builder,
 		openrtb_ext.BidderKayzen:            kayzen.Builder,
 		openrtb_ext.BidderKidoz:             kidoz.Builder,
 		openrtb_ext.BidderKrushmedia:        krushmedia.Builder,
---
+++ openrtb_ext/imp_jwplayer.go
@@ -0,0 +1,6 @@
+package openrtb_ext
+
+type ImpExtJWPlayer struct {
+  PlacementId string `json:"placementId"`
+  PublisherId string `json:"publisherId,omitempty"`
+}
