bf5ec354bb86c44086c306b68bf6460434c613a4
Karim Mourra <karim@jwplayer.com>
2022-06-01 14:12:53 -0400

    removes conflicting fields

--- adapters/jwplayer/jwplayer.go
+++ adapters/jwplayer/jwplayer.go
@@ -12,15 +12,36 @@ import (
 	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
-type adapter struct {
+type JWPlayerAdapter struct {
 	endpoint string
 }
 
 // Builder builds a new instance of the JWPlayer adapter for the given bidder with the given config.
 func Builder(bidderName openrtb_ext.BidderName, config config.Adapter) (adapters.Bidder, error) {
-	bidder := &adapter{
+	bidder := &JWPlayerAdapter{
 		endpoint: config.Endpoint,
 	}
 	return bidder, nil
 }
 
+func (a *JWPlayerAdapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
+    requestCopy := *request
+    for _,imp:= range requestCopy.Imp {
+        placementId := imp.ext.prebid.bidder.jwplayer.placementId
+        imp.tagid = placementId
+    }
+
+    if site := requestCopy.site; site != nil {
+        delete(site, "id")
+
+        if publisher := site.publisher; publisher != nil {
+            delete(publisher, "id")
+        }
+    }
+
+    if app := requestCopy.app; app != nil {
+        delete(app, "id")
+    }
+
+
+}
