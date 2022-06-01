5b89266d33c2542b977e20091876cdbd36e701dc
Karim Mourra <karim@jwplayer.com>
2022-06-01 15:29:16 -0400

    adds notes

--- adapters/jwplayer/jwplayer.go
+++ adapters/jwplayer/jwplayer.go
@@ -23,44 +23,54 @@ func Builder(bidderName openrtb_ext.BidderName, config config.Adapter) (adapters
 }
 
 func (a *JWPlayerAdapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
-    requestCopy := *request
-    for _,imp:= range requestCopy.Imp {
-        placementId := imp.Ext.prebid.bidder.jwplayer.placementId
-        imp.tagid = placementId
-        delete(imp, "ext")
-    }
+	requestCopy := *request
+	for _, imp := range requestCopy.Imp {
+		placementId := imp.Ext.prebid.bidder.jwplayer.placementId
+		imp.TagID = placementId
+		imp.Ext = nil
+	}
 
-    if site := requestCopy.Site; site != nil {
-        delete(site, "id")
+	if site := requestCopy.Site; site != nil {
+		// per Xandr doc, if set, this should equal the Xandr placement code.
+		// It is best to remove, since placement code is set to imp.TagID
+		// https://docs.xandr.com/bundle/supply-partners/page/incoming-bid-request-from-ssps.html#IncomingBidRequestfromSSPs-SiteObjectSiteObject
+		site.ID = ""
 
-        if publisher := site.Publisher; publisher != nil {
-            delete(publisher, "id")
-        }
-    }
+		if publisher := site.Publisher; publisher != nil {
+			// per Xandr doc, if set, this should equal the Xandr publisher code.
+			// Used to set a default placement ID in the auction if tagid, site.id, or app.id are not provided.
+			// It is best to remove, since placement code is set to imp.TagID
+			// https://docs.xandr.com/bundle/supply-partners/page/incoming-bid-request-from-ssps.html#IncomingBidRequestfromSSPs-PublisherObject
+			publisher.ID = ""
+		}
+	}
 
-    if app := requestCopy.App; app != nil {
-        delete(app, "id")
-    }
+	if app := requestCopy.App; app != nil {
+		// per Xandr doc, if set, used to look up an Xandr tinytag ID by tinytag code.
+		// It is best to remove, since Xandr expects an ID specific to its platform
+		// https://docs.xandr.com/bundle/supply-partners/page/incoming-bid-request-from-ssps.html#IncomingBidRequestfromSSPs-AppObjectAppObject
+		app.ID = ""
+	}
 
-    requestJSON, err := json.Marshal(requestCopy)
-    if err != nil {
-        return nil, []error{err}
-    }
+	requestJSON, err := json.Marshal(requestCopy)
+	if err != nil {
+		return nil, []error{err}
+	}
 
-    headers := http.Header{}
-    headers.Add("Content-Type", "application/json;charset=utf-8")
-    headers.Add("Accept", "application/json")
+	headers := http.Header{}
+	headers.Add("Content-Type", "application/json;charset=utf-8")
+	headers.Add("Accept", "application/json")
 
-    requestData := &adapters.RequestData{
-        Method:  "POST",
-        Uri:     a.endpoint,
-        Body:    requestJSON,
-        Headers: headers,
-    }
+	requestData := &adapters.RequestData{
+		Method:  "POST",
+		Uri:     a.endpoint,
+		Body:    requestJSON,
+		Headers: headers,
+	}
 
-    return []*adapters.RequestData{requestData}, nil
+	return []*adapters.RequestData{requestData}, nil
 }
 
 func (a *JWPlayerAdapter) MakeBids(internalRequest *openrtb2.BidRequest, externalRequest *adapters.RequestData, response *adapters.ResponseData) (*adapters.BidderResponse, []error) {
-    return nil, nil
+	return nil, nil
 }
--- adapters/jwplayer/jwplayer_test.go
+++ adapters/jwplayer/jwplayer_test.go
@@ -1,42 +1,42 @@
 package jwplayer
 
 import (
-    "testing"
-    "JWPlayerAdapter"
-    "github.com/prebid/prebid-server/adapters"
-    "github.com/mxmCherry/openrtb/v15/openrtb2"
-    "github.com/stretchr/testify/assert"
+	"JWPlayerAdapter"
+	"github.com/mxmCherry/openrtb/v15/openrtb2"
+	"github.com/prebid/prebid-server/adapters"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestRequest(t *testing.T) {
-    var a JWPlayerAdapter
-    a.URI = "http://test.com/openrtb2"
+	var a JWPlayerAdapter
+	a.URI = "http://test.com/openrtb2"
 
-    var reqInfo adapters.ExtraRequestInfo
-    reqInfo.PbsEntryPoint = "video"
+	var reqInfo adapters.ExtraRequestInfo
+	reqInfo.PbsEntryPoint = "video"
 
-    var req openrtb2.BidRequest
-    req.ID = "test_id"
+	var req openrtb2.BidRequest
+	req.ID = "test_id"
 
-    impExt := `{"prebid":{"bidder":{"jwplayer":{"placementId":123}}}}`
+	impExt := `{"prebid":{"bidder":{"jwplayer":{"placementId":123}}}}`
 
-    req.Imp = append(req.Imp, openrtb2.Imp{ID: "1_0", Ext: []byte(impExt)})
+	req.Imp = append(req.Imp, openrtb2.Imp{ID: "1_0", Ext: []byte(impExt)})
 
-    result, err := a.MakeRequests(&req, &reqInfo)
+	result, err := a.MakeRequests(&req, &reqInfo)
 
-    assert.Empty(t, err, "Errors array should be empty")
-    assert.Len(t, result, 1, "Only one request should be returned")
+	assert.Empty(t, err, "Errors array should be empty")
+	assert.Len(t, result, 1, "Only one request should be returned")
 
-//     var error error
-//     var reqData *openrtb2.BidRequest
-//     error = json.Unmarshal(result[0].Body, &reqData)
-//     assert.NoError(t, error, "Response body unmarshalling error should be nil")
-//
-//     var reqDataExt *appnexusReqExt
-//     error = json.Unmarshal(reqData.Ext, &reqDataExt)
-//     assert.NoError(t, error, "Response ext unmarshalling error should be nil")
-//
-//     regMatch, matchErr := regexp.Match(`^[0-9]+$`, []byte(reqDataExt.Appnexus.AdPodId))
-//     assert.NoError(t, matchErr, "Regex match error should be nil")
-//     assert.True(t, regMatch, "AdPod id doesn't present in Appnexus extension or has incorrect format")
+	//     var error error
+	//     var reqData *openrtb2.BidRequest
+	//     error = json.Unmarshal(result[0].Body, &reqData)
+	//     assert.NoError(t, error, "Response body unmarshalling error should be nil")
+	//
+	//     var reqDataExt *appnexusReqExt
+	//     error = json.Unmarshal(reqData.Ext, &reqDataExt)
+	//     assert.NoError(t, error, "Response ext unmarshalling error should be nil")
+	//
+	//     regMatch, matchErr := regexp.Match(`^[0-9]+$`, []byte(reqDataExt.Appnexus.AdPodId))
+	//     assert.NoError(t, matchErr, "Regex match error should be nil")
+	//     assert.True(t, regMatch, "AdPod id doesn't present in Appnexus extension or has incorrect format")
 }
