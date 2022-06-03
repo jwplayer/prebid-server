14b594aa9e9d0c6eea2e66e6eb5563a74ff187cc
Karim Mourra <karim@jwplayer.com>
2022-06-03 17:56:29 -0400

    aborts when no imps were processed

--- adapters/jwplayer/jwplayer_test.go
+++ adapters/jwplayer/jwplayer_test.go
@@ -1,32 +1,50 @@
 package jwplayer
 
 import (
+	"encoding/json"
 	"github.com/mxmCherry/openrtb/v15/openrtb2"
 	"github.com/prebid/prebid-server/adapters"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestRequest(t *testing.T) {
+
+
+func TestSingleRequest(t *testing.T) {
 	var a JWPlayerAdapter
 	a.endpoint = "http://test.com/openrtb2"
 
 	var reqInfo adapters.ExtraRequestInfo
 	reqInfo.PbsEntryPoint = "video"
 
-	var req openrtb2.BidRequest
-	req.ID = "test_id"
+	request := &openrtb2.BidRequest{
+		ID: "test_id",
+		Imp: []openrtb2.Imp{{
+			ID: "test_imp_id",
+			Ext: json.RawMessage(`{"bidder":{"placementId": "test_placement_id"}}`),
+			Video: &openrtb2.Video{
+				H: 250,
+				W: 350,
+			},
+		}},
+		Site: &openrtb2.Site{},
+	}
 
-	impExt := `{"prebid":{"bidder":{"jwplayer":{"placementId":"123"}}}}`
-
-	req.Imp = append(req.Imp, openrtb2.Imp{ID: "1_0", Ext: []byte(impExt)})
-
-	result, err := a.MakeRequests(&req, &reqInfo)
+	results, err := a.MakeRequests(request, &reqInfo)
 
 	assert.Empty(t, err, "Errors array should be empty")
-	assert.Len(t, result, 1, "Only one request should be returned")
-}
+	assert.Len(t, results, 1, "Only one request should be returned")
 
+	result := results[0]
+	resultJSON := &openrtb2.BidRequest{}
+	json.Unmarshal(result.Body, resultJSON)
+
+	assert.Len(t, resultJSON.Imp, 1, "Imp count should be equal or less than Imps from input. In this test, should be 1.")
+	assert.Empty(t, resultJSON.Imp[0].Ext, "Ext should be deleted")
+	assert.Equal(t, "test_placement_id" , resultJSON.Imp[0].TagID, "placement id should be set to TagID")
+	assert.Equal(t, int(resultJSON.Imp[0].Video.H), 250, "extra metadata in Imp should not be removed")
+	assert.Equal(t, int(resultJSON.Imp[0].Video.W), 350, "extra metadata in Imp should not be removed")
+}
 
 func TestInvalidImpExt(t *testing.T) {
 	var a JWPlayerAdapter
@@ -35,28 +53,62 @@ func TestInvalidImpExt(t *testing.T) {
 	var reqInfo adapters.ExtraRequestInfo
 	reqInfo.PbsEntryPoint = "video"
 
-	var req1 openrtb2.BidRequest
-	req1.ID = "test_id_1"
+	request := &openrtb2.BidRequest{
+		ID: "test_id_1",
+		Imp: []openrtb2.Imp{{
+			ID: "test_imp_id",
+			Ext: json.RawMessage(`{]`),
+		}},
+	}
 
-	impExt1 := `{}`
+	result, err := a.MakeRequests(request, &reqInfo)
 
-	req1.Imp = append(req1.Imp, openrtb2.Imp{ID: "1_0", Ext: []byte(impExt1)})
-
-	result, err := a.MakeRequests(&req1, &reqInfo)
-
-	assert.Len(t, err, 1, "An error should be returned")
+	assert.Len(t, err, 2, "2 errors should be returned")
 	assert.Empty(t, result, "Result should be nil")
+}
 
-		var req2 openrtb2.BidRequest
-		req2.ID = "test_id_2"
+func TestIdsAreRemoved(t *testing.T) {
+	var a JWPlayerAdapter
+	a.endpoint = "http://test.com/openrtb2"
 
-		impExt2 := `{"bidder":{"jwplayer":{"placementId": "123"}}}`
+	var reqInfo adapters.ExtraRequestInfo
+	reqInfo.PbsEntryPoint = "video"
 
-		req2.Imp = append(req2.Imp, openrtb2.Imp{ID: "2_0", Ext: []byte(impExt2)})
+	request := &openrtb2.BidRequest{
+		ID: "test_id",
+		Imp: []openrtb2.Imp{{
+			ID: "test_imp_id",
+			Ext: json.RawMessage(`{"bidder":{"placementId": "test_placement_id"}}`),
+		}},
+		Site: &openrtb2.Site{
+			ID: "test_site_id",
+			Domain: "test_domain",
+			Publisher: &openrtb2.Publisher{
+				ID: "test_publisher_id",
+				Name: "testPublisher_name",
+			},
+		},
+		App: &openrtb2.App{
+			ID: "test_app_id",
+			Domain: "test_app_domain",
+		},
+	}
 
-		result2, err2 := a.MakeRequests(&req2, &reqInfo)
+	results, err := a.MakeRequests(request, &reqInfo)
 
-		assert.Len(t, err2, 1, "An error should be returned")
-		assert.Empty(t, result2, "Result should be nil")
+	assert.Empty(t, err, "Errors array should be empty")
+	assert.Len(t, results, 1, "Only one request should be returned")
 
+	result := results[0]
+	resultJSON := &openrtb2.BidRequest{}
+	json.Unmarshal(result.Body, resultJSON)
+
+	assert.Len(t, resultJSON.Imp, 1, "Imp count should be equal or less than Imps from input. In this test, should be 1.")
+	assert.Empty(t, resultJSON.Imp[0].Ext, "Ext should be deleted")
+	assert.NotEmpty(t, resultJSON.Site, "Site object should not be removed")
+	assert.Empty(t, resultJSON.Site.ID, "Site.id should be removed")
+	assert.NotEmpty(t, resultJSON.Site.Publisher, "Publisher object should not be removed")
+	assert.Empty(t, resultJSON.Site.Publisher.ID, "Publisher.id should be removed")
+	assert.NotEmpty(t, resultJSON.App, "App object should not be removed")
+	assert.Empty(t, resultJSON.App.ID, "App.id should be removed")
 }
