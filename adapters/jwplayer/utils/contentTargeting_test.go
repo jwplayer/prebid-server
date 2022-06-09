3ef2d6b0dceb572eb927a31e57f811a07243f837
Karim Mourra <karim@jwplayer.com>
2022-06-09 17:40:02 -0400

    adds content targ util

---
+++ adapters/jwplayer/utils/contentTargeting_test.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestDemo(t *testing.T) {
+	//D9hUeD6O
+	//Pauwk9RR
+	GetContentTargeting("D9hUeD6O", "http://example.com/media.mp4", "Sample", "Description")
+}
