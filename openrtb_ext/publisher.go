133d45276b65e06a71a96483a5c7f337034a7cb9
Karim Mourra <karim@jwplayer.com>
2022-06-15 23:37:27 -0400

    adds publisher ext

--- openrtb_ext/publisher.go
+++ openrtb_ext/publisher.go
@@ -3,6 +3,7 @@ package openrtb_ext
 // ExtPublisher defines the contract for ...publisher.ext (found in both bidrequest.site and bidrequest.app)
 type ExtPublisher struct {
 	Prebid *ExtPublisherPrebid `json:"prebid"`
+	JWPlayer *JWPlayerPublisher `json:"jwplayer"`
 }
 
 // ExtPublisherPrebid defines the contract for publisher.ext.prebid
@@ -10,4 +11,9 @@ type ExtPublisherPrebid struct {
 	// parentAccount would define the legal entity (publisher owner or network) that has the direct relationship with the PBS
 	// host. As such, the definition depends on the PBS hosting entity.
 	ParentAccount *string `json:"parentAccount,omitempty"`
+}
+
+type JWPlayerPublisher struct {
+	PublisherId string `json:"publisherId"`
+	SiteId string `json:"siteId"`
 }
