28acb80acefd09e7e227bb37e97d3e674f88c9b4
Karim Mourra <karim@jwplayer.com>
2022-07-14 14:19:04 -0400

    updates test to compare against entire json request

--- adapters/jwplayer/jwplayer_test.go
+++ adapters/jwplayer/jwplayer_test.go
@@ -10,8 +10,6 @@ import (
 	"testing"
 )
 
-
-
 func TestSingleRequest(t *testing.T) {
 	var a JWPlayerAdapter
 	a.endpoint = "http://test.com/openrtb2"
@@ -22,7 +20,7 @@ func TestSingleRequest(t *testing.T) {
 	request := &openrtb2.BidRequest{
 		ID: "test_id",
 		Imp: []openrtb2.Imp{{
-			ID: "test_imp_id",
+			ID:  "test_imp_id",
 			Ext: json.RawMessage(`{"bidder":{"placementId": "test_placement_id"}}`),
 			Video: &openrtb2.Video{
 				H: 250,
@@ -41,11 +39,20 @@ func TestSingleRequest(t *testing.T) {
 	resultJSON := &openrtb2.BidRequest{}
 	json.Unmarshal(result.Body, resultJSON)
 
-	assert.Len(t, resultJSON.Imp, 1, "Imp count should be equal or less than Imps from input. In this test, should be 1.")
-	assert.Empty(t, resultJSON.Imp[0].Ext, "Ext should be deleted")
-	assert.Equal(t, "test_placement_id" , resultJSON.Imp[0].TagID, "placement id should be set to TagID")
-	assert.Equal(t, int(resultJSON.Imp[0].Video.H), 250, "extra metadata in Imp should not be removed")
-	assert.Equal(t, int(resultJSON.Imp[0].Video.W), 350, "extra metadata in Imp should not be removed")
+	expectedJSON := &openrtb2.BidRequest{
+		ID: "test_id",
+		Imp: []openrtb2.Imp{{
+			ID:    "test_imp_id",
+			TagID: "test_placement_id",
+			Video: &openrtb2.Video{
+				H: 250,
+				W: 350,
+			},
+		}},
+		Site: &openrtb2.Site{},
+	}
+
+	assert.Equal(t, expectedJSON, resultJSON)
 }
 
 func TestInvalidImpExt(t *testing.T) {
@@ -58,7 +65,7 @@ func TestInvalidImpExt(t *testing.T) {
 	request := &openrtb2.BidRequest{
 		ID: "test_id_1",
 		Imp: []openrtb2.Imp{{
-			ID: "test_imp_id",
+			ID:  "test_imp_id",
 			Ext: json.RawMessage(`{]`),
 		}},
 	}
@@ -79,19 +86,19 @@ func TestIdsAreRemoved(t *testing.T) {
 	request := &openrtb2.BidRequest{
 		ID: "test_id",
 		Imp: []openrtb2.Imp{{
-			ID: "test_imp_id",
+			ID:  "test_imp_id",
 			Ext: json.RawMessage(`{"bidder":{"placementId": "test_placement_id"}}`),
 		}},
 		Site: &openrtb2.Site{
-			ID: "test_site_id",
+			ID:     "test_site_id",
 			Domain: "test_domain",
 			Publisher: &openrtb2.Publisher{
-				ID: "test_publisher_id",
+				ID:   "test_publisher_id",
 				Name: "testPublisher_name",
 			},
 		},
 		App: &openrtb2.App{
-			ID: "test_app_id",
+			ID:     "test_app_id",
 			Domain: "test_app_domain",
 		},
 	}
@@ -114,7 +121,6 @@ func TestIdsAreRemoved(t *testing.T) {
 	assert.NotEmpty(t, resultJSON.App, "App object should not be removed")
 	assert.Empty(t, resultJSON.App.ID, "App.id should be removed")
 }
-
 
 func TestOpenRTBEmptyResponse(t *testing.T) {
 	httpResp := &adapters.ResponseData{
