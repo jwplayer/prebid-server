578c9d119183dc6b1e514e729e94fa620190e224
AndBeyondMediaHB <107686862+AndBeyondMediaHB@users.noreply.github.com>
2022-08-17 20:45:24 +0300

    New Adapter: AndBeyondMedia (#2322)

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -110,6 +110,7 @@ const (
 	BidderAJA               BidderName = "aja"
 	BidderAlgorix           BidderName = "algorix"
 	BidderAMX               BidderName = "amx"
+	BidderAndBeyondMedia    BidderName = "andbeyondmedia"
 	BidderApacdex           BidderName = "apacdex"
 	BidderApplogy           BidderName = "applogy"
 	BidderAppnexus          BidderName = "appnexus"
@@ -273,6 +274,7 @@ func CoreBidderNames() []BidderName {
 		BidderAJA,
 		BidderAlgorix,
 		BidderAMX,
+		BidderAndBeyondMedia,
 		BidderApacdex,
 		BidderApplogy,
 		BidderAppnexus,
