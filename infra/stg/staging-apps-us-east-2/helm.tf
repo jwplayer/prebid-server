ecb85ea8eb063b56dfdffef53fd2809e1fee636d
monica.bota@connatix.com <monica.bota@connatix.com>
2024-09-18 11:47:13 +0300

    test deploy

---
+++ infra/stg/staging-apps-us-east-2/helm.tf
@@ -0,0 +1,16 @@
+module "helm-release-cluster" {
+  source        = "gitlab.ops.connatix.com/connatix/helm-release/deploy"
+  chart_repo    = "http://chartmuseum.ops.connatix.com/"
+  chart_version = "^11.0.0"
+  chart_name    = "cnx-universal-chart"
+  release_name  = "prebid-server"
+  values        = ["${file("helm/values.yaml")}"]
+
+  overrides = {
+    "containerSettings.image.tag" : var.cnx_version
+    "project.url":      var.cnx_project_url
+    "containerSettings.image.repository" : var.cnx_image_repo
+    "replicaCount":     1
+    "ingress.hostname": "prebid-server.stg.connatix.com"
+  }
+}
