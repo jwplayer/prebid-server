b7082fde4df5002ac8b8eb069362c38188780f0c
monica.bota@connatix.com <monica.bota@connatix.com>
2024-10-03 16:12:03 +0300

    fix: log request

--- code/adapters/connatix/connatix.go
+++ code/adapters/connatix/connatix.go
@@ -155,12 +155,8 @@ func splitRequests(imps []openrtb2.Imp, request *openrtb2.BidRequest, uri string
 	headers.Add("Content-Type", "application/json")
 	headers.Add("Accept", "application/json")
 	
-	fmt.Printf("cnx_ipv6: "+ request.Device.IPv6)
-	fmt.Printf("cnx_ip: "+ request.Device.IP)
-	fmt.Println("cnx_ipv6: ", request.Device.IPv6)
-    fmt.Println("cnx_ip: ", request.Device.IP)
-    fmt.Println("cnx_request: ", request)
-	
+    fmt.Println("cnx_request: ", json.Marshal(request))
+    fmt.Println("cnx_uri: ", uri)
 	
     if len(request.Device.UA) > 0 {
         headers.Add("User-Agent", request.Device.UA)
