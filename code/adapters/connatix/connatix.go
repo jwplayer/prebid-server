c359d3733b3ee3cf09f26073c29acb2995c82763
monica.bota@connatix.com <monica.bota@connatix.com>
2025-01-15 13:20:51 +0200

    fix: fix pipe

--- code/adapters/connatix/connatix.go
+++ code/adapters/connatix/connatix.go
@@ -153,6 +153,7 @@ func splitRequests(imps []openrtb2.Imp, request *openrtb2.BidRequest, uri string
 		
 		fmt.Println("cnx_request: ", reqJSON)
         fmt.Println("cnx_uri: ", uri)
+        
 		
 		if err != nil {
 			errs = append(errs, err)
